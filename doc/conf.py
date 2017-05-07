d411061b63c65610080a3e39cdd767f9283d7f26
Juan Chacon-Hurtado <j.chacon@unesco-ihe.org>
2017-05-07 13:32:10 +0200

    documentation update

--- doc/conf.py
+++ doc/conf.py
@@ -47,9 +47,9 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'lumped-hydrological'
-copyright = u'2017, Author'
-author = u'Author'
+project = u'lumphyd'
+copyright = u'2017, Juan Chacon-Hurtado'
+author = u'Juan Chacon-Hurtado'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -84,7 +84,7 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'agogo'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
